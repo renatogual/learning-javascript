239d47513b2ffd3b935e2235b74c0a4501f963c4
eox-tecnologia <eox-tecnologia>
2020-12-21 17:15:41 -0300

    Criando validações

--- validacao-cpf/index.js
+++ validacao-cpf/index.js
@@ -1,37 +1,32 @@
 function validateCPF(cpf) {
-    let formatedCPF = cpf.slice(0, 11).replace(/\D+/g, '');
-    let firstDigitOriginal = cpf.slice(12, 13);
-    let secondDigitOriginal = cpf.slice(13, 14);
+    let formatedCPF = cpf.replace(/\D+/g, '');
 
-    let arrayCPF = formatedCPF.split('');
+    if(typeof cpf === 'undefined') return false;
+    if(formatedCPF.length !== 11) return false;
+
+    let firstDigitOriginal = formatedCPF.slice(9, 10);
+    let secondDigitOriginal = formatedCPF.slice(10, 11);
+
+    let arrayCPF = formatedCPF.slice(0, 9).split('');
 
     let descendingSequenceFirstDigit = descendingSequence(10);
-    let descendingSequenceSecondDigit = descendingSequence(11);
-
     let arrayFirstDigit = [];
-    let arraySecondDigit = [];
-    
-    arrayCPF.forEach((e, i) => {
-        arrayFirstDigit.push(e * descendingSequenceFirstDigit[i]);
-        arraySecondDigit.push(e * descendingSequenceSecondDigit[i]);
-    });
-
+    arrayCPF.forEach((e, i) => arrayFirstDigit.push(e * descendingSequenceFirstDigit[i]));
     let accFirstDigit = arrayFirstDigit.reduce((acc, next) => acc + next);
-    let accSecondDigit = arraySecondDigit.reduce((acc, next) => acc + next);
-
     let firstDigit = 11 - (accFirstDigit % 11);
+
+    let descendingSequenceSecondDigit = descendingSequence(11);
+    let arraySecondDigit = [];
+    let novoArrayCPF = [...arrayCPF, Number(firstDigit)];
+    novoArrayCPF.forEach((e, i) => arraySecondDigit.push(e * descendingSequenceSecondDigit[i]));
+    let accSecondDigit = arraySecondDigit.reduce((acc, next) => acc + next);
     let secondDigit = 11 - (accSecondDigit % 11);
 
-    // if(firstDigit > 9) firstDigit = 0;
-    // if(secondDigit > 9) secondDigit = 0;
+    if(firstDigit > 9) firstDigit = 0;
+    if(secondDigit > 9) secondDigit = 0;
     
-    // if(firstDigitOriginal == firstDigit && secondDigitOriginal == secondDigit) return true;
-    // else return false;
-    console.log('firstOrig', firstDigitOriginal);
-    console.log('secondOrig', secondDigitOriginal);
-
-    console.log('first', firstDigit);
-    console.log('second', secondDigit);
+    if(firstDigitOriginal == firstDigit && secondDigitOriginal == secondDigit) return true;    
+    else return false;
 }
 
 function descendingSequence(number) {
@@ -43,4 +38,7 @@ function descendingSequence(number) {
     return array.reverse();
 }
 
-validateCPF('043.515.721-32');
+const cpf = validateCPF('04351572132');
+
+if(cpf) console.log('CPF Válido !!!');
+else console.log('CPF Inválido !!!');
