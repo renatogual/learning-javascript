41781342c608abe657eba272befb2169c1fefcae
eox-tecnologia <eox-tecnologia>
2020-12-22 11:04:00 -0300

    Refatorando código para padrão Orientado a Objetos

--- validacao-cpf/index.js
+++ validacao-cpf/index.js
@@ -1,44 +1,66 @@
-function validateCPF(cpf) {
-    let formatedCPF = cpf.replace(/\D+/g, '');
-
-    if(typeof cpf === 'undefined') return false;
-    if(formatedCPF.length !== 11) return false;
-
-    let firstDigitOriginal = formatedCPF.slice(9, 10);
-    let secondDigitOriginal = formatedCPF.slice(10, 11);
-
-    let arrayCPF = formatedCPF.slice(0, 9).split('');
-
-    let descendingSequenceFirstDigit = descendingSequence(10);
-    let arrayFirstDigit = [];
-    arrayCPF.forEach((e, i) => arrayFirstDigit.push(e * descendingSequenceFirstDigit[i]));
-    let accFirstDigit = arrayFirstDigit.reduce((acc, next) => acc + next);
-    let firstDigit = 11 - (accFirstDigit % 11);
-
-    let descendingSequenceSecondDigit = descendingSequence(11);
-    let arraySecondDigit = [];
-    let novoArrayCPF = [...arrayCPF, Number(firstDigit)];
-    novoArrayCPF.forEach((e, i) => arraySecondDigit.push(e * descendingSequenceSecondDigit[i]));
-    let accSecondDigit = arraySecondDigit.reduce((acc, next) => acc + next);
-    let secondDigit = 11 - (accSecondDigit % 11);
-
-    if(firstDigit > 9) firstDigit = 0;
-    if(secondDigit > 9) secondDigit = 0;
-    
-    if(firstDigitOriginal == firstDigit && secondDigitOriginal == secondDigit) return true;    
-    else return false;
-}
-
-function descendingSequence(number) {
-    let first = 2;
-    let array = [];
-    for(let i=first; i <= number; i++) {
-        array.push(i);
+class ValidateCPF {
+    constructor(cpf) {
+        this.cpf = cpf.replace(/\D+/g, '');
+        this.firstDigit = this.cpf.slice(-2, -1);
+        this.secondDigit = this.cpf.slice(-1);
+        this.partialCPF = this.cpf.slice(0, 9).split('');
     }
-    return array.reverse();
+
+    checksEntry() {
+        if(typeof this.cpf == 'undefined') return false;
+        if(this.cpf.length != 11) return false;
+        if(this.cpf[0].repeat(11) == this.cpf) return false;
+        return true;
+    }
+
+    findFirstDigitCalculated() {
+        let arrayFirstDigitCalculated = [];
+        this.partialCPF.forEach((element, index) => {
+            arrayFirstDigitCalculated.push(element * this.descendingSequence(10)[index]);
+        })
+        let accFirstDigitCalculated = arrayFirstDigitCalculated.reduce((acc, next) => acc + next);
+        let firstDigitCalculated = 11 - (accFirstDigitCalculated % 11);
+
+        if(firstDigitCalculated > 9) firstDigitCalculated = 0;
+
+        return firstDigitCalculated;
+    }
+
+    findSecondDigitCalculated() {
+        let arraySecondDigitCalculated = [];
+        let newArrayPartialCPF = [...this.partialCPF, this.findFirstDigitCalculated()];
+        newArrayPartialCPF.forEach((element, index) => {
+            arraySecondDigitCalculated.push(element * this.descendingSequence(11)[index]);
+        })
+        let accSecondDigitCalculated = arraySecondDigitCalculated.reduce((acc, next) => acc + next);
+        let secondDigitCalculated = 11 - (accSecondDigitCalculated % 11);
+
+        if(secondDigitCalculated > 9) secondDigitCalculated = 0;
+
+        return secondDigitCalculated;
+    }
+
+    descendingSequence(number) {
+        let first = 2;
+        let array = [];
+        for(let i=first; i <= number; i++) {
+            array.push(i);
+        }
+        return array.reverse();
+    }
 }
 
-const cpf = validateCPF('04351572132');
+function main(value) {
+    const cpf = new ValidateCPF(value);
 
-if(cpf) console.log('CPF Válido !!!');
-else console.log('CPF Inválido !!!');
+    if(!cpf.checksEntry()) {
+        console.log('CPF informado não é válido !');
+    } 
+    else if(cpf.firstDigit == cpf.findFirstDigitCalculated() && cpf.secondDigit == cpf.findSecondDigitCalculated()) {
+        console.log('CPF informado é válido !');
+    } else {
+        console.log('CPF informado não é válido !');
+    }
+}
+
+main('04351572132');
